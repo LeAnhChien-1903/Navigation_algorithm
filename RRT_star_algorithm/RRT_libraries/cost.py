bc85c05d358fb860d9c6227d7a6f3f2a6524f241
Le Anh Chien <76254109+LeAnhChien-1903@users.noreply.github.com>
2022-08-26 15:24:17 +0700

    Update RRT_libraries

--- RRT_star_algorithm/RRT_libraries/cost.py
+++ RRT_star_algorithm/RRT_libraries/cost.py
@@ -1,16 +1,17 @@
-from dis import dis
 import math
+import sys
+sys.path.insert(0, 'RRT_libraries')
+from node import Node
 
 class Cost:
-    @staticmethod
-    def get_distance_and_angle(x, y):
-        dx = y[0] - x[0]
-        dy = y[1] - x[1]
-        return math.hypot(dx, dy), math.atan2(dy, dx)
+    def getDistance(start, end):
+        return math.hypot(end.x - start.x, end.y - start.y)
+    def getAngle(start, end):
+        return math.atan2(end.y - start.y, end.x - start.x)
     def getCostOfPath(self, path):
         cost = 0
         for i in range(len(path)-1):
-            distance, _ = self.get_distance_and_angle(path[i], path[i+1])
+            distance, _ = self.getDistance(path[i], path[i+1])
             cost += distance
         
         return cost
--- RRT_star_algorithm/RRT_libraries/utilsVer2.py
+++ RRT_star_algorithm/RRT_libraries/utilsVer2.py
@@ -8,21 +8,16 @@ import math
 import sys
 
 sys.path.insert(0, 'RRT_libraries')
-import env
 from node import Node
 
 class Utils:
-    def __init__(self, grayImage):
-        self.grayImage = grayImage
+    def __init__(self, binaryImage):
+        self.binaryImage = binaryImage
         self.delta = 0.5
-    def collision(self, start, end):
+    def checkCollision(self, start, end):
         x = list(np.arange(start.x, end.x, (end.x-start.x/100)))
         y = list(((end.y-start.y)/(end.x-start.x))*(x - start.x)+ start.y)
         for i in range(len(x)):
-            if self.grayImage[int(y[i]), int(x[i])] == 0:
+            if self.binaryImage[int(y[i]), int(x[i])] == 0:
                 return True
         return False
-    def getDistance(self, start, end):
-        return math.hypot(end.x - start.x, end.y - start.y)
-    def getAngle(self, start, end):
-        return math.atan2(end.y - start.y, end.x - start.x)
